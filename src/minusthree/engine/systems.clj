49691a14271b7a69b927ba46a989ff9b4e90e690
keychera <kyogatama@gmail.com>
2026-02-27 18:30:28 +0900

    no transform3d

--- src/minusthree/engine/systems.clj
+++ src/minusthree/engine/systems.clj
@@ -3,7 +3,6 @@
    [minusthree.engine.input :as input]
    [minusthree.engine.loading :as loading]
    [minusthree.engine.time :as time]
-   [minusthree.engine.transform3d :as t3d]
    [minusthree.gl.texture :as texture]
    [minusthree.engine.thorbox.thorbox :as thorbox]))
 
@@ -11,7 +10,6 @@
   [time/system
    loading/system
    texture/system
-   t3d/system
    input/system
 
    thorbox/system])
