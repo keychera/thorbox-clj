06df3d8cbd452e5b890f8288a575a682d55626f9
keychera <kyogatama@gmail.com>
2026-02-28 23:59:08 +0900

    disable depth test for a more civilized 2d world

--- src/minusthree/rendering/+core.clj
+++ src/minusthree/rendering/+core.clj
@@ -15,7 +15,7 @@
   (GL45/glEnable GL45/GL_BLEND)
   (GL45/glEnable GL45/GL_CULL_FACE)
   (GL45/glEnable GL45/GL_MULTISAMPLE)
-  (GL45/glEnable GL45/GL_DEPTH_TEST)
+  #_(GL45/glEnable GL45/GL_DEPTH_TEST)
   game)
 
 (defn rendering-zone [game]
@@ -28,14 +28,13 @@
     (GL45/glClear (bit-or GL45/GL_COLOR_BUFFER_BIT GL45/GL_DEPTH_BUFFER_BIT))
     (GL45/glViewport 0 0 w h)
 
-    (atlas/render-world world)
-    ;; why the subsequent render disappear?
-
     (let [{:keys [render-data]} (utils/query-one world ::gradientsky/render-data)
           {::gradientsky/keys [program-info vao]} render-data]
       (GL45/glUseProgram (:program program-info))
       (GL45/glBindVertexArray vao)
       (GL45/glDrawArrays GL45/GL_TRIANGLES 0 6))
+
+    (atlas/render-world world)
 
     game))
 
